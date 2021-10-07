d0b80273a3c9b40e464ab40370a51170658f70c3
root <kevin.t.moralesnguyen@gmail.com>
2021-10-06 20:43:35 -0700

    Implemented some methods for IntStack class

--- src/IntStack.java
+++ src/IntStack.java
@@ -1,74 +1,142 @@
 /*
-    Name: TODO
-    PID:  TODO
+    Name: Kevin Morales-Nguyen
+    PID:  A17186624
  */
 
 import java.util.EmptyStackException;
 
 /**
- * TODO
- * @author TODO
- * @since  TODO
+ * This class is the implementation of a stack that works with primitive integers, it is
+ * implemented by using a integer array that can be resized based on a expand/shrink factor,
+ * there are also various methods to push a pop single or many elements.
+ * @author Kevin Morales Nguyen
+ * @since  10/6/2021
  */
 public class IntStack {
 
     /* instance variables, feel free to add more if you need */
     private int[] data;
     private int nElems;
+    private int init_capacity;
     private double loadFactor;
     private double shrinkFactor;
 
     public IntStack(int capacity, double loadF, double shrinkF) {
-        /* TODO */
+        if(capacity < 5){
+            throw new IllegalArgumentException("Capacity <= 0, Invalid");
+        }
+
+        if(loadF < 0.67 || loadF > 1){
+            throw new IllegalArgumentException("Load factor is outside of valid range");
+        }
+
+        if(shrinkF <= 0 || shrinkFactor >0.33){
+            throw new IllegalArgumentException("Shrink factor is outside of valid range");
+        }
+
+        this.data = new int[capacity];
+        this.nElems = 0;
+        this.init_capacity = capacity;
+        this.loadFactor = loadF;
+        this.shrinkFactor  = shrinkF;
+
     }
 
     public IntStack(int capacity, double loadF) {
-        /* TODO */
+        this(capacity, loadF, 0.25);
     }
 
     public IntStack(int capacity) {
-        /* TODO */
+        this(capacity, 0.75 , 0.25);
     }
 
     public boolean isEmpty() {
-        /* TODO */
-        return false;
+        if(this.nElems == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
     public void clear() {
-        /* TODO */
+        this.data = new int[init_capacity];
+        this.nElems = 0;
     }
 
     public int size() {
-        /* TODO */
-        return 0;
+        return this.nElems;
+
     }
 
     public int capacity() {
-        /* TODO */
-        return 0;
+        return this.data.length;
     }
 
     public int peek() {
-        /* TODO */
-        return 0;
+        return this.data[nElems-1];
     }
 
     public void push(int element) {
-        /* TODO */
+
+        double calc_loadF = ++nElems / (double)this.data.length;
+
+        if(calc_loadF >= loadFactor){
+            this.data = factor_size_copy(2.0);
+            this.data[--nElems] = element;
+        }
+        else{
+            this.data[--nElems] = element;
+        }
+        nElems++;
     }
 
     public int pop() {
-        /* TODO */
-        return 0;
+        nElems--;
+        int temp = this.data[nElems - 1];
+
+        //double shrink_factor =
+        //if(){
+
+        //}
+
+
+
+
+        return temp;
     }
 
     public void multiPush(int[] elements) {
-        /* TODO */
+        nElems+= elements.length;
+
     }
 
     public int[] multiPop(int amount) {
         /* TODO */
         return null;
+    }
+
+
+    private int[] factor_size_copy(double factor){
+
+        int[] temp = new int[(int)(this.data.length * factor)];
+        for(int i = 0; i < this.nElems - 1; i++) {
+            temp[i] = this.data[i];
+        }
+        return temp;
+    }
+
+    public void print_arr_using_size(){
+        for(int i = 0;i< nElems;i++){
+            System.out.print(this.data[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public void print_arr(){
+        for(int i = 0;i< this.data.length;i++){
+            System.out.print(this.data[i] + " ");
+        }
+        System.out.println();
     }
 }
